575bec9442714f9c0d8697a1a9dc1882d123b997
Branky Shao <bishao@ebay.com>
2014-04-27 22:48:19 +0800

    enhance test for issue #5

--- src/test/java/cascading/hcatalog/HCatTapTest.java
+++ src/test/java/cascading/hcatalog/HCatTapTest.java
@@ -26,6 +26,20 @@
  * limitations under the License.
  */
 
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cascading.hcatalog;
 
 import cascading.flow.Flow;
@@ -192,8 +206,10 @@ public class HCatTapTest {
         Flow flow = connector.connect(source, output, pipe);
         flow.complete();
 
-        FileAssert.assertEquals(new File(hcatOut),
+        FileAssert.assertEquals(new File("src/test/resources/data/test_partition_0.txt"),
                 new File(resultPath + "testPartitionsIn/part-00000"));
+        FileAssert.assertEquals(new File("src/test/resources/data/test_partition_1.txt"),
+                new File(resultPath + "testPartitionsIn/part-00001"));
     }
 
     @Test
